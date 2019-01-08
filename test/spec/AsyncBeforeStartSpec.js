a7f5426a24e4dd7be9614537841083e9ad0abca8
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2019-01-08 11:52:04 +0100

    feat(boundaries): support asychBefore and asyncAfter

---
+++ test/spec/AsyncBeforeStartSpec.js
@@ -0,0 +1,91 @@
+import '../TestHelper';
+
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import transactionBoundariesModule from '../..';
+
+import { forEach } from 'min-dash';
+
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
+
+
+describe('transaction-boundaries-async-before-start', function() {
+
+  var diagram = require('./async-before-start.bpmn');
+
+  function withModeler(config, fn) {
+
+    return function(done) {
+
+      var modeler = new Modeler(config);
+
+      modeler.importXML(diagram, function(err) {
+        if (err) {
+          done(err);
+        }
+
+        modeler.invoke(fn);
+
+        done();
+      });
+
+    };
+  }
+
+
+  function inject(fn) {
+
+    var config = {
+      additionalModules: [ transactionBoundariesModule ],
+      moddleExtensions: {
+        camunda: camundaModdleDescriptor
+      }
+    };
+
+    return withModeler(config, fn);
+  }
+
+
+  describe('API', function() {
+
+    it('should show', inject(function(transactionBoundaries, overlays) {
+
+      // when
+      transactionBoundaries.show();
+
+      // then
+      expect(overlays.get({ type: 'transaction-boundaries' })).to.have.length(2);
+
+    }));
+
+  });
+
+
+  describe('unit tests', function() {
+
+    it('get transaction boundary elements', inject(function(transactionBoundaries, elementRegistry) {
+
+      // given
+      var expectedBoundaries = {
+        'StartEvent_1': { before: true, after: false },
+        'EndEvent_1': { before: false, after: true }
+      };
+
+      // when
+      var transactionBoundaryElements = transactionBoundaries._getTransactionBoundaryElements();
+
+      // then
+      expect(transactionBoundaryElements).to.have.length(2);
+
+      forEach(transactionBoundaryElements, function(element) {
+
+        var boundaries = expectedBoundaries[element.shape.id];
+
+        expect(boundaries).to.eql(element.boundaries);
+      });
+
+    }));
+
+  });
+
+});
